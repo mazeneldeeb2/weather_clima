d4e1eb064f46a38bd1df85e499a794b88a343d2f
mazeneldeeb2 <mazeneldeeb9@gmail.com>
2023-09-18 19:39:27 +0300

    Initial Commit

---
+++ weather_clima/weather_climaApp.swift
@@ -0,0 +1,17 @@
+//
+//  weather_climaApp.swift
+//  weather_clima
+//
+//  Created by mazen eldeeb on 18/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct weather_climaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
